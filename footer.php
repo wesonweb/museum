1d7a36450ce14831cea3d54a68d109fd824a2bf1
Wes Mehaffy <wesonweb@gmail.com>
2018-11-19 21:30:01 +0000

    html changes to footer

--- footer.php
+++ footer.php
@@ -10,11 +10,33 @@
  */
 ?>
 	<footer id="colophon" class="site-footer clearfix" role="contentinfo">
-		<div class="site-info container">
-			<section class="legal-info">
-				<p><?php echo theme_copyright(); ?> Chesham museum | Registered Charity Number: 1172671 | <a href="https://cheshammuseum.org.uk/privacy-policy/" class="footer-link">view our privacy policy</a></p>
-			</section>
+
+		<div class="container">
+			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/lottery-logo.png" width="300" alt="lottery-funded-logo">
+		</div>
+
+		<div class="site-info">
+			<div class="container">
+				
+				<!-- footer menu -->
+				<nav aria-controls="secondary-menu">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'menu-2',
+							'menu_id'        => 'secondary-menu',
+							'walker'				 => new Clean_Walker_Nav
+						) );
+					?>
+				</nav>
+
+				<!-- copyright info -->
+				<section class="legal-info">
+					<p><?php echo theme_copyright(); ?> Chesham museum | Registered Charity Number: 1172671 | <a href="https://cheshammuseum.org.uk/privacy-policy/" class="footer-link">view our privacy policy</a></p>
+				</section>
+
+			</div>
 		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 <script src="<?php bloginfo('template_url'); ?>/js/cmscripts.js"></script>
 <?php wp_footer(); ?>
