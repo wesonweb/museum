172f40bc3b21e635a03ce205d149fa437674f215
Wes Mehaffy <wesonweb@gmail.com>
2018-11-20 20:58:39 +0000

    tweaks to sponsor logo area

--- page-home.php
+++ page-home.php
@@ -99,7 +99,7 @@
 
 
 		<div class="container sponsor-details">
-			<?php get_template_part('template-parts/news'); ?>
+			<?php //get_template_part('template-parts/news'); ?>
 			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/lottery-logo.png" width="" alt="lottery-funded-logo">
 		</div>
 
