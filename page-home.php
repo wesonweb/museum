4c4c1b78653448743e4020d88270a0a2628b221a
Wes Mehaffy <wesonweb@gmail.com>
2018-11-28 19:34:08 +0000

    removed bullet points

--- page-home.php
+++ page-home.php
@@ -84,10 +84,7 @@
 			<?php get_template_part('template-parts/news'); ?>
 		</section><!-- .news-wrapper-->
 
-		<div class="container sponsor-details">
-			<?php //get_template_part('template-parts/news'); ?>
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/lottery-logo.png" width="200" alt="lottery-funded-logo">
-		</div>
+		
 
 	</main><!-- .site-main -->
 <?php get_footer(); ?>
---
+++ template-parts/sponsor-logo.php
@@ -0,0 +1,4 @@
+<div class="container sponsor-details">
+  <?php //get_template_part('template-parts/news'); ?>
+  <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/lottery-logo.png" width="200" alt="lottery-funded-logo">
+</div>
